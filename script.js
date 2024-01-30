87ca0f32b5fdbdaece197a7aff3f8a7929af9523
jose-deltoro <montesdeltorojosearmando@gmail.com>
2024-01-30 09:39:03 -0500

    funcion eliminar y completar tarea

--- script.js
+++ script.js
@@ -8,3 +8,26 @@ function agregar() {
     tareas.push({ indicator, description, completed: false });
     console.log('Tarea añadida.');
   }
+
+  function eliminar() {
+    const index = readline.question('Índice de la tarea a eliminar: ');
+    if (index >= 0 && index < tareas.length) {
+      tareas.splice(index, 1);
+      console.log('Tarea eliminada.');
+    } else {
+      console.log('Índice no válido.');
+    }
+  }
+
+  function completar() {
+    const index = readline.question('Índice de la tarea completada: ');
+    if (index >= 0 && index < tareas.length) {
+      tareas[index].completed = true;
+      console.log('Tarea marcada como completada.');
+    } else {
+      console.log('Índice no válido.');
+    }
+  }
+
+  
+  
